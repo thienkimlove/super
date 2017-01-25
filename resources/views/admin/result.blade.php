bb930840f714cfca7df9034613515aa667f3346d
Quan Do <manhquan.do@ved.com.vn>
2017-01-25 03:41:12 +0000

    update

--- resources/views/admin/result.blade.php
+++ resources/views/admin/result.blade.php
@@ -189,6 +189,7 @@
                                 <th>SubId</th>
                                 <th>Device</th>
                                 <th>Location</th>
+                                <th>Time</th>
                             </tr>
                             </thead>
                             <tbody>
@@ -202,7 +203,7 @@
                                     <td>{{$click->hash_tag}}</td>
                                     <td>{{ ($click->offer_allow_devices) ? config('devices')[$click->offer_allow_devices] : '' }}</td>
                                     <td>{{$click->offer_geo_locations}}</td>
-
+                                    <td>{{$click->created_at->format('d/m/Y')}}</td>
                                 </tr>
                             @endforeach
                             </tbody>
