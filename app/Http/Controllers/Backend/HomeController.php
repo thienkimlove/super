e9dcf1d18c1bd9bc7a25d0425421e2890f3dc291
Quan Do <manhquan.do@ved.com.vn>
2017-02-28 02:59:07 +0000

    update

--- app/Http/Controllers/Backend/HomeController.php
+++ app/Http/Controllers/Backend/HomeController.php
@@ -578,22 +578,97 @@ class HomeController extends AdminController
         $offers = json_decode(file_get_contents($feed_url), true);
 
         if (isset($offers['offers'])) {
-            foreach ($offers['offers'] as $offer) {
+            $total = count($offers['offers']);
+            if ($total > 0) {
+                foreach ($offers['offers'] as $offer) {
 
-                $devices = 1;
+                    $devices = 1;
+                    $isIphone = false;
+                    $isIpad = false;
+                    $android = false;
+                    $ios = false;
+                    if ($offer['devices']) {
+                        foreach ($offer['devices'] as $device) {
+                            if (strpos(strtolower($device['device_type']), 'iphone') !== false) {
+                                $isIphone = true;
+                            }
+                            if (strpos(strtolower($device['device_type']), 'ipad') !== false) {
+                                $isIpad = true;
+                            }
+                            if (strpos(strtolower($device['device_type']), 'droid') !== false) {
+                                $android = true;
+                            }
+
+                            if ($isIphone && $isIpad) {
+                                $ios = true;
+                            }
+                        }
+                    }
+
+
+                    if ($ios && $android) {
+                        $devices = 2;
+                    } else if ($android) {
+                        $devices = 4;
+                    } else if ($ios) {
+                        $devices = 5;
+                    } else if ($isIphone) {
+                        $devices = 6;
+                    } else if ($isIpad) {
+                        $devices = 7;
+                    }
+
+                    $countries = [];
+
+                    foreach ($offer['countries'] as $country) {
+                        $countries[]  = $country['code'];
+                    }
+
+                    Offer::updateOrCreate(['net_offer_id' => $offer['id']], [
+                        'net_offer_id' => $offer['id'],
+                        'name' => str_limit( $offer['name'], 250),
+                        'redirect_link' => $offer['tracking_link'].'&s1=#subId',
+                        'click_rate' => round(floatval($offer['payout'])/intval(env('RATE_CRON')), 2),
+                        'allow_devices' => $devices,
+                        'geo_locations' => implode(',', $countries),
+                        'network_id' => $network->id,
+                        'status' => true,
+                        'auto' => true
+                    ]);
+                }
+            }
+
+        } else {
+            $total = 0;
+        }
+
+        return 'Total Offers : '.$total;
+    }
+
+    private function cpway($network)
+    {
+        //$url = 'http://intrexmedia.com/api.php?key=758be7ff505de4ad';
+        $feed_url = $network->cron;
+        $offers = json_decode(file_get_contents($feed_url), true);
+        $total = count($offers);
+
+        if ($total > 0) {
+            foreach ($offers as $offer) {
+
+                $devices = null;
                 $isIphone = false;
                 $isIpad = false;
                 $android = false;
                 $ios = false;
                 if ($offer['devices']) {
                     foreach ($offer['devices'] as $device) {
-                        if (strpos(strtolower($device['device_type']), 'iphone') !== false) {
+                        if (strpos(strtolower($device), 'iphone') !== false) {
                             $isIphone = true;
                         }
-                        if (strpos(strtolower($device['device_type']), 'ipad') !== false) {
+                        if (strpos(strtolower($device), 'ipad') !== false) {
                             $isIpad = true;
                         }
-                        if (strpos(strtolower($device['device_type']), 'droid') !== false) {
+                        if (strpos(strtolower($device), 'droid') !== false) {
                             $android = true;
                         }
 
@@ -616,86 +691,22 @@ class HomeController extends AdminController
                     $devices = 7;
                 }
 
-                $countries = [];
-
-                foreach ($offer['countries'] as $country) {
-                    $countries[]  = $country['code'];
-                }
-
-                Offer::updateOrCreate(['net_offer_id' => $offer['id']], [
-                    'net_offer_id' => $offer['id'],
-                    'name' => str_limit( $offer['name'], 250),
-                    'redirect_link' => $offer['tracking_link'].'&s1=#subId',
-                    'click_rate' => round(floatval($offer['payout'])/intval(env('RATE_CRON')), 2),
+                Offer::updateOrCreate(['net_offer_id' => $offer['offer_id']], [
+                    'net_offer_id' => $offer['offer_id'],
+                    'name' => str_limit( $offer['offer_name'], 250),
+                    'redirect_link' => str_replace('&s1=&s2=&s3=', '&s1=#subId', $offer['tracking_url']),
+                    'click_rate' => round(floatval(str_replace('$', '', $offer['rate']))/intval(env('RATE_CRON')), 2),
                     'allow_devices' => $devices,
-                    'geo_locations' => implode(',', $countries),
+                    'geo_locations' => implode(',', $offer['geos']),
                     'network_id' => $network->id,
                     'status' => true,
                     'auto' => true
                 ]);
+
             }
         }
 
-        return 'Total Offers : '.count($offers);
-    }
-
-    private function cpway($network)
-    {
-        //$url = 'http://intrexmedia.com/api.php?key=758be7ff505de4ad';
-        $feed_url = $network->cron;
-        $offers = json_decode(file_get_contents($feed_url), true);
-        foreach ($offers as $offer) {
-
-            $devices = null;
-            $isIphone = false;
-            $isIpad = false;
-            $android = false;
-            $ios = false;
-            if ($offer['devices']) {
-                foreach ($offer['devices'] as $device) {
-                    if (strpos(strtolower($device), 'iphone') !== false) {
-                        $isIphone = true;
-                    }
-                    if (strpos(strtolower($device), 'ipad') !== false) {
-                        $isIpad = true;
-                    }
-                    if (strpos(strtolower($device), 'droid') !== false) {
-                        $android = true;
-                    }
-
-                    if ($isIphone && $isIpad) {
-                        $ios = true;
-                    }
-                }
-            }
-
-
-            if ($ios && $android) {
-                $devices = 2;
-            } else if ($android) {
-                $devices = 4;
-            } else if ($ios) {
-                $devices = 5;
-            } else if ($isIphone) {
-                $devices = 6;
-            } else if ($isIpad) {
-                $devices = 7;
-            }
-
-            Offer::updateOrCreate(['net_offer_id' => $offer['offer_id']], [
-                'net_offer_id' => $offer['offer_id'],
-                'name' => str_limit( $offer['offer_name'], 250),
-                'redirect_link' => str_replace('&s1=&s2=&s3=', '&s1=#subId', $offer['tracking_url']),
-                'click_rate' => round(floatval(str_replace('$', '', $offer['rate']))/intval(env('RATE_CRON')), 2),
-                'allow_devices' => $devices,
-                'geo_locations' => implode(',', $offer['geos']),
-                'network_id' => $network->id,
-                'status' => true,
-                'auto' => true
-            ]);
-
-        }
-        return 'Total Offers : '.count($offers);
+        return 'Total Offers : '.$total;
     }
 
     public function cron()
@@ -705,11 +716,11 @@ class HomeController extends AdminController
         foreach ($networks as $network) {
             if ($network->cron) {
                 if ($network->type == 'onetulip') {
-                    $message .= $this->feed($network).'"\n"';
                     $message .= 'Network :' . $network->name . 'is type onetulip have cron='.$network->cron.'"\n"';
+                    $message .= $this->feed($network).'"\n"';
                 } else if ($network->type == 'cpway') {
-                    $message .= $this->cpway($network).'"\n"';
                     $message .= 'Network :' . $network->name . 'is type cpway have cron='.$network->cron.'"\n"';
+                    $message .= $this->cpway($network).'"\n"';
                 }
             } else {
                 $message .= 'Network :' . $network->name . ' has no cron'.'"\n"';
