57a77b07f4cc13d653c897514090635c70fa0c2a
Quan Do <manhquan.do@ved.com.vn>
2017-03-06 07:59:08 +0000

    update

--- app/Http/Controllers/Frontend/MainController.php
+++ app/Http/Controllers/Frontend/MainController.php
@@ -253,7 +253,7 @@ class MainController extends Controller
                 ->join('networks', 'network_clicks.network_id', '=', 'networks.id')
                 ->join('clicks', 'network_clicks.sub_id', '=', 'clicks.hash_tag')
                 ->join('users', 'clicks.user_id', '=', 'users.id')
-                ->select('offers.name', 'offers.id', 'clicks.created_at as click_at', 'network_clicks.ip', 'network_clicks.created_at', 'users.username', 'network_clicks.id as postback_id', 'network.name as network_name')
+                ->select('offers.name', 'offers.id', 'clicks.created_at as click_at', 'network_clicks.ip', 'network_clicks.created_at', 'users.username', 'network_clicks.id as postback_id', 'networks.name as network_name')
                 ->orderBy('network_clicks.id', 'desc')
                 ->limit(10)
                 ->get();
