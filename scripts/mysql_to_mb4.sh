9999eb0cc78ffa69f381477c047a44b72e7ec2b0
Quan Do <quan.dm@teko.vn>
2017-12-15 06:26:17 +0000

    update

--- scripts/mysql_to_mb4.sh
+++ scripts/mysql_to_mb4.sh
@@ -6,12 +6,12 @@ DATABASE=$1
 MYSQL="mysql -uroot -ptieungao -N --database=$DATABASE"
 
 # convert database with all tables to utf8mb4
-echo "ALTER DATABASE $DATABASE CHARACTER SET = utf8mb4 COLLATE = utf8mb4_bin;" | $MYSQL
+echo "ALTER DATABASE $DATABASE CHARACTER SET = utf8mb4 COLLATE = utf8mb4_unicode_ci;" | $MYSQL
 
 echo 'show tables' | eval $MYSQL | while read table
 do
     echo "ALTER TABLE $table ROW_FORMAT=DYNAMIC;" |
         eval $MYSQL
-    echo "ALTER TABLE $table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;" |
+    echo "ALTER TABLE $table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" |
         eval $MYSQL
 done
