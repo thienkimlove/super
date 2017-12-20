54b735672308d2c4a69e979bcea90434954d7955
Quan Do <quan.dm@teko.vn>
2017-12-20 03:50:22 +0000

    update

--- app/Console/Commands/CorrectLead.php
+++ app/Console/Commands/CorrectLead.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\NetworkClick;
+use DB;
 use Illuminate\Console\Command;
 
 class CorrectLead extends Command
@@ -38,7 +38,7 @@ class CorrectLead extends Command
      */
     public function handle()
     {
-        $oldLeads = NetworkClick::join('clicks', 'network_clicks.sub_id', '=', 'clicks.hash_tag')
+        $oldLeads = DB::connection('richxyz')->table('network_clicks')->join('clicks', 'network_clicks.sub_id', '=', 'clicks.hash_tag')
             ->join('offers', 'clicks.offer_id', '=', 'offers.id')
             ->selectRaw('network_clicks.id as lead_id, network_clicks.network_id as lead_network_id, clicks.id as click_id, offers.id as offer_id, network_clicks.sub_id as lead_sub_id, network_clicks.amount as lead_amount')
             ->whereNull('network_clicks.offer_id')
@@ -46,7 +46,7 @@ class CorrectLead extends Command
             ->get();
 
         foreach ($oldLeads as $oldLead) {
-            NetworkClick::where('id', $oldLead->lead_id)->update([
+            DB::connection('richxyz')->table('network_clicks')->where('id', $oldLead->lead_id)->update([
                 'offer_id' => $oldLead->offer_id,
                 'click_id' => $oldLead->click_id,
                 'json_data' => json_encode(['subid' => $oldLead->lead_sub_id, 'amount' => $oldLead->lead_amount*2, 'network_id' => $oldLead->lead_network_id])
